612ab08fb99e70595c46d3eab6eba44d5fbbf5f7
Illia1044 <92012282+Illia1044@users.noreply.github.com>
2023-02-03 18:18:42 +0200

    Delete bot directory

--- bot/common/Constants.py
+++
@@ -1,7 +0,0 @@
-class Constants(object):
-    botToken: str = "5934033684:AAGdydIavf-U0TMK7qGjI2xpGO4y1saTDlM"
-
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Constants, cls).__new__(cls)
-        return cls.instance
