f3fe90eb019ce382b61bb48bbf5133e6fa42904c
Illia1044 <92012282+Illia1044@users.noreply.github.com>
2023-02-22 11:38:45 +0200

    Delete common directory

--- common/Constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Constants(object):
-    botToken = os.environ['BOT_TOKEN']
-
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Constants, cls).__new__(cls)
-        return cls.instance
